44f35b961a93c83556f106c0556a0d58e793bfaa
sean1093 <sean1093@gmail.com>
2016-10-01 23:00:11 +0800

    update 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # html-rwd-table
-A RWD-based html table extend tool which made by pure JavaScript and CSS
+A RWD-based html table extend tool which made by pure JavaScript and CSS, you don't need to add any other dependency libaraies.
+
+### Getting Started:
+When you want to use RWD html table extension, you need to include both CSS file and JavaScript file.
+
+```html
+<link href="css/rwd-table-extend.css" rel="stylesheet">
+<script src="js/rwd-table-extend.js"></script>
+```
+
+First, you need to create a <code>div</code> with id.
+```html
+<div id="myTable"></div>
+```
---
+++ js/rwd-table-extend.js
@@ -0,0 +1,74 @@
+/**
+ * rwd-table-extend.js
+ * A RWD-based html table extend tool which made by pure JavaScript and CSS
+ * see: https://github.com/sean1093/html-rwd-table for details
+ * @version: v1.0.0
+ * @author: Sean Chou
+ * Licensed under MIT (https://github.com/sean1093/html-rwd-table/blob/master/LICENSE)
+ */
+
+(function () {
+	"use strict";
+	
+	//public 
+	var rwdTableExtend = function(divId) {
+		this.divId = divId;
+		this.div = document.getElementById(divId);
+	};
+	rwdTableExtend.prototype.initTable = function() {
+		var target = document.getElementById(this.divId);
+		var table = document.createElement("table");
+		table.id = "rwd-table";
+		table.className = "rwd-table";
+		var thead = document.createElement("thead");
+		var tbody = document.createElement("tbody");
+		table.appendChild(thead);
+		table.appendChild(tbody);
+		target.appendChild(table);	
+	};
+	rwdTableExtend.prototype.addTableHead = function(array) {
+		this.table = document.getElementById("rwd-table");
+		this.thead = this.table.getElementsByTagName('thead')[0];
+        var hRow = this.thead.insertRow(0);              
+        for(var idx in array){
+            var header = document.createElement("th");
+            header.innerHTML = array[idx];
+            hRow.appendChild(header);
+        }
+    };
+    rwdTableExtend.prototype.updateHead = function(array) {
+    	if(this.thead === undefined || this.thead === null) {
+    		console.error("[rwd-table-extend] Please add table header first!");
+    	}
+    	else {
+    		var hRow = this.thead.deleteRow(0); 
+            this.addTableHead(array);
+    	}
+    };
+    rwdTableExtend.prototype.addTableRow = function(data) {
+    	this.table = document.getElementById("rwd-table");
+        this.tbody = this.table.getElementsByTagName('tbody')[0];
+        var dRow = this.tbody.insertRow(this.tbody.rows.length);
+        var idx = 0;
+        for(var key in data){
+            var cell = dRow.insertCell(idx);
+            cell.innerHTML = data[key];
+            cell.setAttribute("data-th",key);
+            idx ++;
+        } 
+    };
+    rwdTableExtend.prototype.deleteRow = function(idx) {
+		document.getElementById("rwd-table").deleteRow(idx);
+    };
+    rwdTableExtend.prototype.hideContent = function(key) {
+    	var css = document.createElement("style");
+		css.type = "text/css";
+		css.innerHTML = "@media screen and (max-width:451px) { .rwd-table td[data-th='"+key+"'] { display: none; }}";
+		document.body.appendChild(css);
+    };
+
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+    	module.exports = rwdTableExtend;
+ 	else
+    	window.rwdTableExtend = rwdTableExtend;
+})();
