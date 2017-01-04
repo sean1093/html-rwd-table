30a45ce0880c943b076dbe2264f360abb35acfc4
sean1093 <sean1093@gmail.com>
2017-01-04 22:51:17 +0800

    update

--- js/rwd-table-extend.js
+++ js/rwd-table-extend.js
@@ -4,31 +4,31 @@
  * see: https://github.com/sean1093/html-rwd-table for details
  * @version: v1.0.0
  * @author: Sean Chou
- * Licensed under MIT (https://github.com/sean1093/html-rwd-table/blob/master/LICENSE)
+ * @licensed: under MIT (https://github.com/sean1093/html-rwd-table/blob/master/LICENSE)
  */
 
 (function () {
-	"use strict";
-	
-	//public 
-	var rwdTableExtend = function(divId) {
-		this.divId = divId;
-		this.div = document.getElementById(divId);
-	};
-	rwdTableExtend.prototype.initTable = function() {
-		var target = document.getElementById(this.divId);
-		var table = document.createElement("table");
-		table.id = "rwd-table";
-		table.className = "rwd-table";
-		var thead = document.createElement("thead");
-		var tbody = document.createElement("tbody");
-		table.appendChild(thead);
-		table.appendChild(tbody);
-		target.appendChild(table);	
-	};
-	rwdTableExtend.prototype.addTableHead = function(array) {
-		this.table = document.getElementById("rwd-table");
-		this.thead = this.table.getElementsByTagName('thead')[0];
+    "use strict";
+    
+    //public 
+    var rwdTableExtend = function(divId) {
+        this.divId = divId;
+        this.div = document.getElementById(divId);
+    };
+    rwdTableExtend.prototype.initTable = function() {
+        var target = document.getElementById(this.divId);
+        var table = document.createElement("table");
+        table.id = "rwd-table";
+        table.className = "rwd-table";
+        var thead = document.createElement("thead");
+        var tbody = document.createElement("tbody");
+        table.appendChild(thead);
+        table.appendChild(tbody);
+        target.appendChild(table);  
+    };
+    rwdTableExtend.prototype.addTableHead = function(array) {
+        this.table = document.getElementById("rwd-table");
+        this.thead = this.table.getElementsByTagName('thead')[0];
         var hRow = this.thead.insertRow(0);              
         for(var idx in array){
             var header = document.createElement("th");
@@ -37,16 +37,16 @@
         }
     };
     rwdTableExtend.prototype.updateHead = function(array) {
-    	if(this.thead === undefined || this.thead === null) {
-    		console.error("[rwd-table-extend] Please add table header first!");
-    	}
-    	else {
-    		var hRow = this.thead.deleteRow(0); 
+        if(this.thead === undefined || this.thead === null) {
+            console.error("[rwd-table-extend] Please add table header first!");
+        }
+        else {
+            var hRow = this.thead.deleteRow(0); 
             this.addTableHead(array);
-    	}
+        }
     };
     rwdTableExtend.prototype.addTableRow = function(data, locate) { // data, row index
-    	this.table = document.getElementById("rwd-table");
+        this.table = document.getElementById("rwd-table");
         this.tbody = this.table.getElementsByTagName('tbody')[0];
 
         var rowIdx = this.tbody.rows.length;
@@ -56,24 +56,30 @@
         var dRow = this.tbody.insertRow(rowIdx);
         var idx = 0;
         for(var key in data){
-            var cell = dRow.insertCell(idx);
+            var cell = dRow.insertCell(idx);    
             cell.innerHTML = data[key];
             cell.setAttribute("data-th",key);
+            cell.setAttribute("contenteditable",true);
             idx ++;
         } 
     };
-    rwdTableExtend.prototype.deleteRow = function(idx) {
-		document.getElementById("rwd-table").deleteRow(idx);
+    rwdTableExtend.prototype.deleteRow = function(idx, count) {
+        if(count === undefined) count = 1;
+        while(count>0){
+          document.getElementById("rwd-table").deleteRow(idx);
+          count--;  
+        }
+        
     };
     rwdTableExtend.prototype.hideContent = function(key) {
-    	var css = document.createElement("style");
-		css.type = "text/css";
-		css.innerHTML = "@media screen and (max-width:451px) { .rwd-table td[data-th='"+key+"'] { display: none; }}";
-		document.body.appendChild(css);
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = "@media screen and (max-width:451px) { .rwd-table td[data-th='"+key+"'] { display: none; }}";
+        document.body.appendChild(css);
     };
 
-	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
-    	module.exports = rwdTableExtend;
- 	else
-    	window.rwdTableExtend = rwdTableExtend;
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+        module.exports = rwdTableExtend;
+    else
+        window.rwdTableExtend = rwdTableExtend;
 })();
